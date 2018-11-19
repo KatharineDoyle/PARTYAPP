af49242e698603d08ae4332d0c2044f6a89eef26
Katharine Doyle <katharinecdoyle@gmail.com>
2018-11-19 16:46:18 +0100

    Added devise user features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # def is_current_user_admin?
+  #   if user_signed_in? && current_user.admin?
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
 end
