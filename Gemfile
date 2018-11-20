f41fdfd7e31eadbee24a91dd5c1f035897335a4e
Samuel Beca <samuel.beca@gmail.com>
2018-11-20 17:51:56 +0100

    started on image uploader

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'redis'
 gem 'devise'
 gem "pundit"
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '1.9.1'
+gem 'carrierwave', '~> 1.2'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- app/models/space.rb
+++ app/models/space.rb
@@ -5,6 +5,8 @@ class Space < ApplicationRecord
   validates :availability, presence: true, inclusion: { in: [true, false] }
   validates :capacity, numericality: { only_integer: true }
 
+  mount_uploader :photo, PhotoUploader
+
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
   belongs_to :user
