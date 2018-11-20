7b8e81176e5d28dbb94c795cb93c2f6cea2f1572
Katharine Doyle <katharinecdoyle@gmail.com>
2018-11-20 18:03:33 +0100

    Fixed space_id on bookings

--- db/migrate/20181120170128_add_space_id_to_bookings.rb
+++ db/migrate/20181120170128_add_space_id_to_bookings.rb
@@ -1,5 +1,6 @@
 class AddSpaceIdToBookings < ActiveRecord::Migration[5.2]
   def change
-    add_column :bookings, :space_id, :integer
+    add_column :bookings, :space_id, :string
+    add_index :bookings, :space_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_161647) do
+ActiveRecord::Schema.define(version: 2018_11_20_170128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 2018_11_20_161647) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "space_id"
+    t.string "space_id"
+    t.index ["space_id"], name: "index_bookings_on_space_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
