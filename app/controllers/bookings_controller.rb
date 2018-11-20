359f35ffe17e4ce03dd7f4ce3a9d8479d97f4d59
Katharine Doyle <43238918+KatharineDoyle@users.noreply.github.com>
2018-11-20 17:39:09 +0100

    Check it out! (#14)
    * Added methods to the booking controller
    * Fixed something in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,25 @@
 class BookingsController < ApplicationController
-  def new
-    @booking = Booking.new
+  before_action :find_and_authorize_current_booking, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @user = current_user
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_and_authorize_current_booking
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :theme, :user_id, :space_id)
   end
 end
