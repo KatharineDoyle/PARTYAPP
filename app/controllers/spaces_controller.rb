277d0b356c296608569423c377de1b6e9532022e
Samuel Beca <samuel.beca@gmail.com>
2018-11-20 10:47:54 +0100

    added spaces controller CRUD

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,28 @@
 class SpacesController < ApplicationController
+  def index
+    @spaces = Space.all
+  end
+
+  def show
+    @space = Space.find(params[:id])
+    @space = Space.new
+  end
+
+  def new
+    @space = Space.new
+  end
+
+  def create
+    @space = Space.new(space_params)
+    if @space.save
+      redirect_to @space
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def space_params
+    params.require(:space).permit(:name, :address, :capacity, :availabilty, :photo)
+  end
 end
