6bb8b689979c8609d3323e4f57f8d28464440ce6
Katharine Doyle <43238918+KatharineDoyle@users.noreply.github.com>
2018-11-21 15:00:48 +0100

    Creating bookings (#25)
    * Fixed space_id on bookings
    
    * Fixes to bookings form
    
    * Added aspects to bookings
    
    * Changed parts of bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2018_11_20_170128) do
 ActiveRecord::Schema.define(version: 2018_11_21_095345) do
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
