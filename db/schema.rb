0e4b5d390f08208372b1961a88a7826ada8cf430
Katharine Doyle <katharinecdoyle@gmail.com>
2018-11-21 20:56:14 +0100

    small change

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 2018_11_21_095345) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "space_id"
-    t.index ["space_id"], name: "index_bookings_on_space_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
