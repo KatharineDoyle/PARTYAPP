0c5494c9226e8fbf80738f680ab7b54539a953ae
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-11-22 12:43:36 +0100

    Adding space_id to bookings

--- db/migrate/20181122100903_add_space_id_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddSpaceIdToBookings < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :bookings, :space, foreign_key: true
-  end
-end
--- db/migrate/20181122104019_add_space_id_to_bookings.rb
+++ db/migrate/20181122104019_add_space_id_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddSpaceIdToBookings < ActiveRecord::Migration[5.2]
   def change
-    add_reference :bookings, :space_id, foreign_key: true
+    add_reference :bookings, :space, foreign_key: true
   end
 end
