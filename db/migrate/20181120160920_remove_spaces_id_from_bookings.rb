5d233e6bd8e59b3dc1fc545fe1442571704fcc90
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-11-22 07:40:12 +0100

    Creating reviews (#33)
    * Modified form for space
    
    * Adding new app/views/spaces/index.hmtl.erb
    
    * Add rating to Reviews
    
    * Adding Reviews controller and views
    
    * Adding style from Sam to views/spaces
    
    * Fix cards on indes.html.erb
    
    * Fix cards only html
    
    * Changes to reviews
    
    * Modified space/show.html.erb
    
    * Change in identation for show.html.erb
    
    * Changes to previous show.hmtl.erb
    
    * Final change to push

--- db/migrate/20181120160920_remove_spaces_id_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSpacesIdFromBookings < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :bookings, :spaces_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_095345) do
+ActiveRecord::Schema.define(version: 2018_11_21_133218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 2018_11_21_095345) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "space_id"
-    t.index ["space_id"], name: "index_bookings_on_space_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
