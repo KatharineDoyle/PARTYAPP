e80ec1a336a8c380d73ccdd6f944cf7a912fcfbb
Samuel Beca <samuel.beca@gmail.com>
2018-11-23 04:54:06 +0100

    color and form, svg test

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -21,7 +21,7 @@ if (mapElement) { // only build a map if there's a div#map to inject into
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/light-v9'
   });
   const markers = JSON.parse(mapElement.dataset.markers);
 
